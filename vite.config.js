885feb8ee9eadcbac9b3ab612c0f2268ed39fe3d
fourshireswindows-png <fourshireswindows@gmail.com>
2026-05-28 18:24:20 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'app.js',
+        chunkFileNames: 'chunk-[name].js',
+        assetFileNames: 'asset-[name][extname]',
+      },
+    },
+  },
 })
