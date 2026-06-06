841d6b260eb48dc4e83d99abf27c8bad7649cdbf
Dazzlar <heymonkeyltd@gmail.com>
2026-06-06 11:58:32 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Build from source (not pre-built bundle)
 export default defineConfig({
   plugins: [react()],
   build: {
